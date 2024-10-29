8b095c566f57e3c57619ff7cfc8d191cae9bd23b
Grigoriy-V <grigoriyvoyakinwork@gmail.com>
2024-10-29 20:21:21 +0700

    Add files via upload

---
+++ Scene_for_script/run.bat
@@ -0,0 +1,1 @@
+hython run_houdini.py
---
+++ Scene_for_script/run_houdini.py
@@ -0,0 +1,25 @@
+import subprocess
+import os
+import sys
+
+houdini_lib_path = "C:/Program Files/Side Effects Software/Houdini 20.5.278/houdini/python3.11libs"
+houdini_bin_path = "C:/Program Files/Side Effects Software/Houdini 20.5.278/bin"
+sys.path.append(houdini_lib_path)
+os.add_dll_directory(houdini_bin_path)
+
+import hou
+
+try:
+    hou.hipFile.load("scene_for_script.hip")
+except hou.LoadWarning as e:
+    print(e)
+
+import hqueue.houdini as hq 
+
+node = hq.getNode("/obj/test_landscape/topgeometry1") 
+disp_node = node.displayNode() 
+disp_node.executeGraph(False, True, False)
+
+node = hq.getNode("/obj/test_landscape/topnet1") 
+disp_node = node.displayNode() 
+disp_node.executeGraph(False, True, False)
